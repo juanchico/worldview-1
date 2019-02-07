d17f295298c739a7f9aca2c8c411cf1a7cb34f0b
KA <kareemalasmar@gmail.com>
2019-02-07 13:40:19 -0800

    schema

---
+++ models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    country: {type: String, required: true},
+    age: {type: Number, required: true},
+    faveFood: {type: String, required: false},
+    faveSong: {type: String, required: false},
+    favePlace: {type: String, required: false},
+    funFact: {type: String, required: false},
+    followers: {type: Array, required: false},
+    following: {type: Array, required: false},
+    countriesVisited: {type: Array, required: false}
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports= User;
