557d5b3518a4ac2405802cb73131091fbf7b97fd
KA <kareemalasmar@gmail.com>
2019-02-08 13:21:31 -0800

    broken route

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,10 @@ const User = require("../models/User.js");
 
 router.post("/api/User", function(req, res) {
   // as long as req.body matches what the model expects, this should insert into the database
+  console.log(req.body);
   User.create(req.body)
-  .then(() => {
-    res.json(true);
+  .then((result) => {
+    res.json(result);
   })
   .catch((err) => {
     // if not, we can at least catch the errr
@@ -13,13 +14,26 @@ router.post("/api/User", function(req, res) {
   });
 });
 
-router.get("/api/Users", function(req, res) {
-  // find all Users where quantity is greater than zero
-  User.find({})
-  .then((docs) => {
-    res.json(docs);
-  });
-});
+// router.get("/api/Users", function(req, res) {
+//   // find all Users where quantity is greater than zero
+//   User.find({})
+//   .then((docs) => {
+//     console.log(docs);
+//     res.json(docs);
+//   });
+// });
+
+// router.put("/api/products/:id", function(req, res) {
+//   Product.findOneAndUpdate({
+//     upc: req.params.id
+//   })
+//   .then((docs) => {
+//     res.json(docs);
+//   })
+//   .catch((err) => {
+//     res.json(err);
+//   });
+// });
 
 
 
