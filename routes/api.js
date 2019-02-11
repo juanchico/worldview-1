bb72522b810e1445f6ca1a0296b632608ff4820b
KA <kareemalasmar@gmail.com>
2019-02-11 09:44:19 -0800

    update route

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User.js");
 
+// creates new user document
 router.post("/api/User", function(req, res) {
   // as long as req.body matches what the model expects, this should insert into the database
   User.create(req.body)
@@ -13,6 +14,7 @@ router.post("/api/User", function(req, res) {
   });
 });
 
+// populates followers for documents
 router.get("/api/Users", function(req, res) {
   // find all Users where quantity is greater than zero
   User.find({}).populate("followers")
@@ -21,17 +23,17 @@ router.get("/api/Users", function(req, res) {
   });
 });
 
+
+// find users with specific country 
 router.get("/api/Country/:name", function(req, res) {
   // find all Users where quantity is greater than zero
-  console.log("working");
-  console.log(req.params.name);
   User.find({country: req.params.name}).populate("followers")
   .then((docs) => {
     res.json(docs);
   });
 });
 
-
+// find user with id
 router.get("/api/User/:id", function(req, res) {
   // find all Users where quantity is greater than zero
   console.log("working");
@@ -40,6 +42,28 @@ router.get("/api/User/:id", function(req, res) {
     res.json(docs);
   });
 });
+
+// // for updating profile
+// router.put("/api/User", function(req, res) {
+//   // as long as req.body matches what the model expects, this should insert into the database
+//   User.findByIdAndUpdate(req.body._id, )
+//   .then((result) => {
+//     res.json(result);
+//   })
+//   .catch((err) => {
+//     // if not, we can at least catch the errr
+//     res.json(err);
+//   });
+// });
+
+// for finding one and displaying 
+// router.get("/api/Users", function(req, res) {
+//   // find all Users where quantity is greater than zero
+//   User.find({}).populate("followers")
+//   .then((docs) => {
+//     res.json(docs);
+//   });
+// });
 
 
 
